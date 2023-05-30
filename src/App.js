e3efc4a999663e6d74287a162aecc97d0f9b9711
Yechiel Klein <kleinyechiel@gmail.com>
2023-05-30 18:42:42 -0400

    created createBook, editBook and deleteBook functions

--- src/App.js
+++ src/App.js
@@ -6,12 +6,24 @@ import BookCreate from './components/BookCreate';
 const App = () => {
 	const [books, setBooks] = useState([]);
 
+	const createBook = (title) => {
+		console.log(`Book with Title of '${title}' Created`);
+	};
+
+	const editBook = (id, title) => {
+		console.log(`Book with id of ${id} updated title: ${title}`);
+	};
+
+	const deleteBook = id => {
+		console.log(`Book with id of ${id} deleted`);
+	};
+	
 	return (
 		<div>
 			<h1>Books</h1>
 			<hr />
-			<BookList />
-			<BookCreate />
+			<BookList onEdit={editBook} onDelete={deleteBook} />
+			<BookCreate onCreate={createBook} />
 		</div>
 	);
 };
